e26059e77822aa7d384fcdc4f6f3bd88bc570d83
Saransh Singh <saranshsingh.it2021@nsec.ac.in>
2024-04-05 18:06:20 +0530

    App is almost ready

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { removeTodo } from '../features/todo/todoSlice'
+import {removeTodo} from '../features/todo/todoSlice'
 
 function Todos() {
-
+    const todos = useSelector((state) => state.todos)
     const dispatch = useDispatch()
-    const todos = useSelector(state => state.todos)
 
   return (
     <>
